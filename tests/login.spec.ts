caeab5ee23f838c37d193a0630d8d9aa637e8b16
scanopy <sylviacanopy@gmail.com>
2026-07-18 12:11:25 +0700

    disable login valid ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
 
-test('User successfully login using valid credential', async ({ page }) => {
+//disable due to credential not able to use
+// test('User successfully login using valid credential', async ({ page }) => {
   
-  //Precondition  
-  const loginPage = new LoginPage(page)
-  await page.goto('https://www.emra.chat/login');
+//   //Precondition  
+//   const loginPage = new LoginPage(page)
+//   await page.goto('https://www.emra.chat/login');
   
-  //Steps
-  await loginPage.loginAs ('testingemrachat@gmail.com','tester!3')
+//   //Steps
+//   await loginPage.loginAs ('testingemrachat@gmail.com','tester!3')
   
-  //Expected Result
-  await expect(page.getByRole('button', { name: 'Fadhli Maulidri Baru' })).toBeVisible();
-});
+//   //Expected Result
+//   await expect(page.getByRole('button', { name: 'Fadhli Maulidri Baru' })).toBeVisible();
+// });
 
 test('User unsuccessfully login using invalid credential', async ({ page }) => {
   
