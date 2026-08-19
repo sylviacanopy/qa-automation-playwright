b755673f6601e4e8035d254f50bc2eb68e562153
scanopy <sylviacanopy@gmail.com>
2026-08-19 10:48:14 +0700

    add login validation testcase

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -18,18 +18,18 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: parseInt(`${process.env.RETRY}`),
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.WORKER ? parseInt(`${process.env.WORKER}`, 10) : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on-first-retry',     // Alat debug spesifik
+    screenshot: 'only-on-failure',
+    // video: 'retain-on-failure',
+    headless: false
   },
 
   /* Configure projects for major browsers */
@@ -39,15 +39,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,28 +1,40 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
 
-//disable due to credential not able to use
-// test('User successfully login using valid credential', async ({ page }) => {
-  
-//   //Precondition  
-//   const loginPage = new LoginPage(page)
-//   await page.goto('https://www.emra.chat/login');
-  
-//   //Steps
-//   await loginPage.loginAs ('testingemrachat@gmail.com','tester!3')
-  
-//   //Expected Result
-//   await expect(page.getByRole('button', { name: 'Fadhli Maulidri Baru' })).toBeVisible();
-// });
-
-test('User unsuccessfully login using invalid credential', async ({ page }) => {
+test('Login successfully with valid email and password', async ({ page }) => {
   
   //Precondition  
   const loginPage = new LoginPage(page)
   await page.goto('https://www.emra.chat/login');
   
   //Steps
-  await loginPage.loginAs ('canopy@gmail.com','tester!1234')
+  await loginPage.loginAs ('sylviacanopy@gmail.com','tester!3')
+  
+  //Expected Result
+  await expect(page.getByRole('button', { name: 'Sylvia Canopy' })).toBeVisible();
+});
+
+test('Login fails with valid email and incorrect password', async ({ page }) => {
+  
+  //Precondition  
+  const loginPage = new LoginPage(page)
+  await page.goto('https://www.emra.chat/login');
+  
+  //Steps
+  await loginPage.loginAs ('sylviacanopy@gmail.com','tester!1234')
+  
+  //Expected Result
+  await expect(page.getByText('Invalid credentials')).toBeVisible();
+});
+
+test('Login fails with unregistered email', async ({ page }) => {
+  
+  //Precondition  
+  const loginPage = new LoginPage(page)
+  await page.goto('https://www.emra.chat/login');
+  
+  //Steps
+  await loginPage.loginAs ('sylviacanopy1@gmail.com','tester!3')
   
   //Expected Result
   await expect(page.getByText('Invalid credentials')).toBeVisible();
